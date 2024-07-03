496e1ccbe04486f80f400252cfab21dc475e2cc7
SVM-Robot <silvano.vento97@gmail.com>
2024-07-03 20:01:14 +0200

    c

---
+++ ag3.js
@@ -0,0 +1,25 @@
+import { spawn } from 'child_process';
+
+const runScript = (script) => {
+  const process = spawn('node', [script]);
+
+  process.stdout.on('data', (data) => {
+    console.log(`stdout from ${script}: ${data}`);
+  });
+
+  process.stderr.on('data', (data) => {
+    console.error(`stderr from ${script}: ${data}`);
+  });
+
+  process.on('close', (code) => {
+    console.log(`${script} process exited with code ${code}`);
+  });
+};
+
+const runScript0 = (script) => {
+    const process = spawn('node', [script]);
+  };
+
+  
+runScript0('ag1.js');
+runScript0('ag2.js');
